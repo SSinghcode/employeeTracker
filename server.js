c33cf547ec0f9106a3d83ef0ea265d1457e57d93
navjot turna <shamsherturna101@gmail.com>
2021-11-08 20:35:33 -0500

    inquirers setup

--- server.js
+++ server.js
@@ -8,6 +8,70 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const db = mysql.createConnection(
- 'mysql://vo1k563fjcq1mght:xzkapkb7cgrqsspb@x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/snpf7b5yeg3jcrem'
+    'mysql://vo1k563fjcq1mght:xzkapkb7cgrqsspb@x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/snpf7b5yeg3jcrem'
 );
 
+const chooseQuestion = [
+    {
+        type: "list",
+        message: "What you choosing ?",
+        name: "choices",
+        choices: [
+            "View all the departments",
+            "View all the roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role",
+            "Exit App",
+        ],
+    },
+];
+
+const menustart = () => {
+
+    inquirer.prompt(chooseQuestion).then(async (answer) => {
+        const answers = answer.choices;
+        switch (answers) {
+            case "View all the departments":
+
+                const departments = await viewDepts();
+                console.table(departments[0]);
+                menustart()
+                break;
+            case "View all the roles":
+
+                const role = await viewDepts();
+                console.table(role[0]);
+                menustart()
+                break;
+
+            case "View all employees":
+
+                const employees = await viewDepts();
+                console.table(employees[0]);
+                menustart()
+                break;
+
+            case "Add a role":
+
+                const addrole = await viewDepts();
+                console.table(addrole[0]);
+                menustart()
+                break;
+            case "Add an employee":
+
+                const addemployee = await viewDepts();
+                console.table(addemployee[0]);
+                menustart()
+                break;
+            case "Update an employee role":
+
+                const updateemployee = await viewDepts();
+                console.table(updateemployee[0]);
+                menustart()
+                break;
+            default:
+                process.exit();
+        }})}
