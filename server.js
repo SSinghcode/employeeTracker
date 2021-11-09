c626ca06f80905ac70a88fa32c29aef91c68b945
navjot turna <shamsherturna101@gmail.com>
2021-11-08 20:01:11 -0500

    employeejs

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-{\rtf1}
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+ 'mysql://vo1k563fjcq1mght:xzkapkb7cgrqsspb@x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/snpf7b5yeg3jcrem'
+);
+
